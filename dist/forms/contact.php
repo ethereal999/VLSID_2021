18ba2501ab10d484e296dee7fb5656817af2e8d4
ethereal999 <nishanksid999@gmail.com>
2020-05-05 14:23:04 +0530

    Added What's New

--- dist/forms/contact.php
+++ dist/forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'vlsid2021@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,7 +17,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
@@ -31,6 +31,12 @@
     'port' => '587'
   );
   */
+  $contact->invalid_to_email = 'Email to (receiving email address) is empty or invalid!';
+  $contact->invalid_from_name = 'From Name is empty!';
+  $contact->invalid_from_email = 'Email from: is empty or invalid!';
+  $contact->invalid_subject = 'Subject is too short or empty!';
+  $contact->short = 'is too short or empty!'; // If the length check number is set and the provided message text is under the set length in the add_message() method call
+  $contact->ajax_error = 'Sorry, the request should be an Ajax POST'; // If ajax property is set true and the post method is not an AJAX call
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
